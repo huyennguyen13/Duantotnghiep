5ffcc8d2af4f9b2ac1ccead3b48ccf5cd4049236
huyennguyen13 <huyennguyen131204101@gmail.com>
2026-02-16 15:44:10 +0700

    fix TrangChu mapping

--- src/main/java/com/poly/petshop/Controller/TranChuController.java
+++ src/main/java/com/poly/petshop/Controller/TranChuController.java
@@ -1,20 +1,3 @@
-package com.poly.petshop.Controller;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.poly.petshop.Dao.SanPhamDao;
-import com.poly.petshop.Entity.SanPhamEntity;
-import com.poly.petshop.Service.SreachService;
-
-import jakarta.servlet.http.HttpSession;
-
 @Controller
 public class TranChuController {
 
@@ -26,8 +9,14 @@ public class TranChuController {
         return "redirect:/customer/TrangChu";
     }
 
+    @GetMapping("/TrangChu")
+    public String redirectTrangChu() {
+        return "redirect:/customer/TrangChu";
+    }
+
     @GetMapping("/customer/TrangChu")
     public String Trangchu(@RequestParam(required = false) String error, Model model) {
+
         if (error != null) {
             model.addAttribute("error","bạn không có quyền truy cập");
         }
@@ -39,14 +28,17 @@ public class TranChuController {
                 .collect(Collectors.toList());
 
         model.addAttribute("lowStockProducts", limitedRandomProducts);
+
         return "views/TrangChu";
     }
 
     @GetMapping("/employee/TrangQuanTri")
     public String Trangquantri(@RequestParam(required = false) String error, Model model) {
+
         if (error != null) {
             model.addAttribute("error", "Bạn không có quyền truy cập");
         }
+
         return "views/TrangQuanTri";
     }
 }
